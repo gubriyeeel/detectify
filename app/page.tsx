d335474d5be912f812701b8740cd3d4781d449f1
Gabriel Rupert Roy Sufrir <officialgabrielsufrir@gmail.com>
2024-01-22 02:42:42 +0800

    feat: camera integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,39 @@
+"use client"
+
+import React, { useRef, useState } from 'react'
+import Webcam from "react-webcam"
+
 type Props = {}
 
 const HomePage = (props: Props) => {
+  const webcamRef = useRef<Webcam>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  // States
+  const [mirrored, setMirrored] = useState<boolean>(false)
+
   return (
-    <div>HomePage</div>
+    <div className="flex h-screen">
+      {/* Left Division - Camera and Canvas */}
+      <div className="relative">
+        <div className="relative h-screen w-full">
+          <Webcam
+            ref={webcamRef}
+            mirrored={mirrored}
+            className="h-full w-full object-contain p-2"
+          />
+          <canvas 
+            ref={canvasRef}
+            className="absolute top-0 left-0 h-full w-full object-contain"
+          >
+          
+          </canvas>
+        </div>
+      </div>
+
+      {/* Right Division - Button Panel and Wiki Section */}
+      
+    </div>
   );
 }
 
