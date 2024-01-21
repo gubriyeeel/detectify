c8c025ff22f0062aae53ec1f5336ae9b7a1a98fd
Gabriel Rupert Roy Sufrir <officialgabrielsufrir@gmail.com>
2024-01-22 05:20:16 +0800

    feat: added object detection

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Camera, FlipHorizontal, PersonStanding, Video, Volume } from "lucide-react";
-import { toast } from "sonner";
-
-import React, { useRef, useState } from "react";
-import Webcam from "react-webcam";
-import { Rings } from "react-loader-spinner";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Slider } from "@/components/ui/slider";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Camera, FlipHorizontal, Moon, PersonStanding, Sun, Video, Volume2 } from "lucide-react";
+import { toast } from "sonner";
+import { Rings } from "react-loader-spinner";
+import { beep } from "@/utils/audio";
+
+import React, { useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { ObjectDetection } from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-cpu"
+import "@tensorflow/tfjs-backend-webgl"
 
 type Props = {};
 
@@ -23,6 +28,20 @@ const HomePage = (props: Props) => {
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false)
   const [volume, setVolume] = useState(0.8)
+  const [model, setModel] = useState<ObjectDetection>();
+
+  useEffect(() => {
+    initModel();
+  }, [])
+
+  // Model Loader
+  // Set in state variable
+
+  async function initModel() {
+    const loadedModel: ObjectDetection = await cocossd.load();
+
+    setModel(loadedModel)
+  }
 
   return (
     <div className="flex h-screen">
@@ -42,7 +61,7 @@ const HomePage = (props: Props) => {
       </div>
 
       {/* Right Division - Button Panel and Wiki Section */}
-      <div className="flex flex-row flex-1">
+      <div className="flex flex-row flex-1 pt-2 pr-2 pb-2">
         <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
           {/* Top Section */}
           <div className="flex flex-col gap-2">
@@ -78,44 +97,50 @@ const HomePage = (props: Props) => {
               <Video />
             </Button>
 
-            <Separator className="my-2" />
-
             <Button
               variant={autoRecordEnabled ? "destructive" : "outline"}
               size={"icon"}
               onClick={toggleAutoRecord}
             >
-              {autoRecordEnabled ? <Rings color="white" height={40} /> : <PersonStanding />}
+              {autoRecordEnabled ? (
+                <Rings color="white" height={40} />
+              ) : (
+                <PersonStanding />
+              )}
             </Button>
+
+            <Separator className="my-2" />
           </div>
           {/* Bottom Section */}
           <div className="flex flex-col gap-2">
             <Separator className="my-2" />
 
             <Popover>
-              <PopoverTrigger>
-                <Button
-                  variant={"outline"}
-                  size={"icon"}
-                >
-                  <Volume />
+              <PopoverTrigger asChild>
+                <Button variant={"outline"} size={"icon"}>
+                  <Volume2 />
                 </Button>
               </PopoverTrigger>
               <PopoverContent>
-                <Slider 
+                <Slider
                   max={1}
                   min={0}
                   step={0.1}
                   defaultValue={[volume]}
                   onValueCommit={(value) => {
-                    setVolume(value[0])
-                    beep(value[0])
+                    setVolume(value[0]);
+                    beep(value[0]);
                   }}
                 />
               </PopoverContent>
             </Popover>
-
           </div>
+        </div>
+
+        {/* Wiki Section */}
+
+        <div className="h-full flex-1 py-4 px-2 overflow-y-scroll">
+          <Features />
         </div>
       </div>
     </div>
@@ -152,6 +177,85 @@ const HomePage = (props: Props) => {
 
   }
 
-};
+  // Inner Components
+
+  function Features() {
+    return <div className="text-sm text-muted-foreground pl-2">
+      <ul className="space-y-4">
+        <li>
+          <strong>Dark Mode/Sys Theme 🌗</strong>
+          <p>Toggle between dark mode and system theme.</p>
+          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+            <Sun size={14} />
+          </Button>{" "}
+          /{" "}
+          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+            <Moon size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Horizontal Flip ↔️</strong>
+          <p>Adjust horizontal orientation.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={() => {
+              setMirrored((prev) => !prev)
+            }}
+          ><FlipHorizontal size={14} /></Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Take Pictures 📸</strong>
+          <p>Capture snapshots at any moment from the video feed.</p>
+          <Button
+            className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={userPromptScreenshot}
+          >
+            <Camera size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Manual Video Recording 📽️</strong>
+          <p>Manually record video clips as needed.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+            onClick={userPromptRecord}
+          >
+            <Video size={14} />
+          </Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Enable/Disable Auto Record 🚫</strong>
+          <p>
+            Option to enable/disable automatic video recording whenever
+            required.
+          </p>
+          <Button className='h-6 w-6 my-2'
+            variant={autoRecordEnabled ? 'destructive' : 'outline'}
+            size={'icon'}
+            onClick={toggleAutoRecord}
+          >
+            <PersonStanding size={14} />
+          </Button>
+        </li>
+
+        <li>
+          <strong>Volume Slider 🔊</strong>
+          <p>Adjust the volume level of the notifications.</p>
+        </li>
+        <li>
+          <strong>Camera Feed Highlighting 🎨</strong>
+          <p>
+            Highlights persons in{" "}
+            <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+            <span style={{ color: "#00B612" }}>green</span>.
+          </p>
+        </li>
+      </ul>
+    </div>
+  }
+}
 
 export default HomePage;
